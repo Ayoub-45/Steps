3d113e278b6c17873bf8ed83fda530b1656e18f4
unknown <ayoubnasraoui161@yahoo.fr>
2023-07-23 23:08:47 +0200

    finish the project

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./styles.css"
 import {App} from "./components/App"
 console.log(App)
 const root = ReactDOM.createRoot(document.getElementById("root"));
